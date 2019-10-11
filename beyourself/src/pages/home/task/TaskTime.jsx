85362da9ec720e212b7fb7e913cca2ca3e66a677
Zoey_evanism <892481573@qq.com>
2019-10-11 23:18:56 +0800

    pu

--- beyourself/src/pages/home/task/TaskTime.jsx
+++ beyourself/src/pages/home/task/TaskTime.jsx
@@ -1,8 +1,33 @@
 import React, { Component } from 'react'
 
+import { DatePicker, List } from 'antd-mobile';
+// import enUs from 'antd-mobile/lib/date-picker/locale/en_US';
+
 import {BodyContainer, TaskTimeContainer, BoderEmptyContainer} from './StyledTask'
 
+const nowTimeStamp = Date.now();
+const now = new Date(nowTimeStamp);
+// GMT is not currently observed in the UK. So use UTC now.
+const utcNow = new Date(now.getTime() + (now.getTimezoneOffset() * 60000));
+
+// Make sure that in `time` mode, the maxDate and minDate are within one day.
+let minDate = new Date(nowTimeStamp - 1e7);
+const maxDate = new Date(nowTimeStamp + 1e7);
+// console.log(minDate, maxDate);
+if (minDate.getDate() !== maxDate.getDate()) {
+  // set the minDate to the 0 of maxDate
+  minDate = new Date(maxDate.getFullYear(), maxDate.getMonth(), maxDate.getDate());
+}
+
 export default class TaskTime extends Component {
+  state = {
+    date: now,
+    time: now,
+    utcDate: utcNow,
+    dpValue: null,
+    customChildValue: null,
+    visible: false,
+  }
   render() {
     return (
       <BodyContainer>
@@ -16,6 +41,19 @@ export default class TaskTime extends Component {
           </div>
           <BoderEmptyContainer></BoderEmptyContainer>
         </TaskTimeContainer>
+
+        <List className="date-picker-list" style={{ backgroundColor: 'white' }}>
+          <DatePicker
+            mode="time"
+            minDate={minDate}
+            maxDate={maxDate}
+            value={this.state.time}
+            onChange={time => this.setState({ time })}
+          >
+            <List.Item arrow="horizontal">Limited time</List.Item>
+          </DatePicker>
+        </List>
+
       </BodyContainer>
     )
   }
