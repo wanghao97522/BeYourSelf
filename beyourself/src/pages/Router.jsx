af05b639e0719b53d2caea3bb7164ec3feb3f9f0
qqqw123 <1097174280@qq.com>
2019-10-23 19:36:52 +0800

    qiumingzhu

--- beyourself/src/pages/Router.jsx
+++ beyourself/src/pages/Router.jsx
@@ -29,6 +29,7 @@ import NewHabit from './growing/views/newhabit/NewSoundContainer';
 import SoundHabit from './growing/views/soundhabit/SoundHabitContainer';
 import SoundTip from './growing/views/soundtip/SoundTipContainer';
 import TaskTime from './growing/views/tasktime/TaskTimeContianer';
+import Selecttime from './growing/views/selecttime/SelectTimeContainer';
 //profile的组件
 import Setting from './profile/setting/Setting'
 import AboutUs from './profile/aboutUs/AboutUs'
@@ -79,7 +80,7 @@ export default class MyRouter extends Component {
                     <Route path="/soundtip" component={SoundTip}></Route>
                     <Route path="/tasktime" component={TaskTime}></Route>
                     <Route path='/index' component={Index} ></Route>
-
+                    <Route path='/selecttime' component={Selecttime}></Route>
                     {/* profile的路由 */}
                     <Route path='/setting' component={Setting} ></Route>
                     <Route path='/aboutus' component={AboutUs}></Route>
@@ -99,8 +100,6 @@ export default class MyRouter extends Component {
                     <Route path='/registersucc' component={RegSucc}></Route>
                     <Route path='/findpwd' component={FindPwd}></Route>
                     <Route path='/code' component={FindPwd}></Route>
-
-                    <Link to={{ path : ' /verification ' , state : { name : 'sunny' }}}></Link>
                     
                     <Redirect from='/' exact to='/index/home'></Redirect> 
                     <Route component={Page404} ></Route>
--- beyourself/src/pages/growing/views/tasktime/TaskTimeUI.jsx
+++ beyourself/src/pages/growing/views/tasktime/TaskTimeUI.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {withRouter} from 'react-router-dom';
-import { PickerView} from 'antd-mobile';
+import { PickerView,Toast} from 'antd-mobile';
 import {TaskTimeStyContainer,Bottom,Wrapper} from './tasktime';
+import querystring from 'querystring';
+import http from '../../../../utils/http1';
 export default withRouter((props)=>{
+    const [value,setvalue]=useState(["0","小时","3","分钟"])
+    // let hId=props.location.query.hid;
+    async function setTime(){
+        let data={
+            // hId,
+            tTimespan:value[0]*60*60+value[2]*60
+        }
+        let result =await http.post({
+            url:'http://10.9.20.181:8084/api/habit/update/tasktime',
+            method:'POST',
+            data:querystring.stringify(data)
+        })
+        if(result.flag){
+            Toast.success('时间调整成功', 1);
+        }else{
+            Toast.fail('时间调整失败失败', 1);
+        }
+        setTimeout(()=>{
+            props.history.goBack();
+        },2000)  
+    }
+    function changeValue(data){
+      setvalue(data);
+    }
     return (
         <TaskTimeStyContainer>
             <div className="title">
@@ -11,14 +37,13 @@ export default withRouter((props)=>{
                     <span>任务时间长短</span>
                     <span>项目</span>
                 </div>
-                <span className="finish">完成</span>
+                <span className="finish" onClick={setTime}>完成</span>
             </div>
             <Bottom>
                 <Wrapper>
                     <PickerView
-                        onChange={(value)=>props.onChange(value)}
-                        // onScrollChange={this.onScrollChange}
-                        value={["0","小时","3","分钟"]}
+                        onChange={(value)=>{changeValue(value)}}
+                        value={value}
                         data={props.datalist}
                         cascade={false}
                     />
